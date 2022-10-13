127ef7fe54f99d4a3364f6c077b272b781470963
Stephen Harvey <itsteveharvey@gmail.com>
2022-10-13 22:46:58 +1300

    Modified signup.html and app.js, form can now take submissions and send to Mailchimp account when replacing place holding X & Ys with API key and Audience ID

--- app.js
+++ app.js
@@ -1,13 +1,57 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request =require("request");
+const https =require("https");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+// Mailchimp URL replace X with the number found on the end of your API Key eg. us4, then replace YYYY with your audience ID.
+  const url = "https://usX.api.mailchimp.com/3.0/lists/YYYY";
+
+// In auth: Change to your name and replace XXXX with your generate API Key.
+  const options = {
+    method: "POST",
+    auth: "stephen1:XXXX"
+  };
+
+const request = https.request(url,options, function(response) {
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+request.write(jsonData);
+request.end();
+
 });
 
 app.listen(3000, function() {
